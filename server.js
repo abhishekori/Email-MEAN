21d39c995c44dcf0352747f3e2b4ea5c29879d40
abhishekori <abhikori1994@gmail.com>
2015-10-27 23:51:04 +0530

    finished basic front end ui

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var nodemailer=require('nodemailer');
 var app=express();
 
@@ -28,3 +28,35 @@ transporter.sendMail(mailOptions,function(error,info){
 app.listen(3000,function(){
 console.log("yes");	
 })
+*/
+var nodemailer = require('nodemailer');
+
+// create reusable transporter object using SMTP transport
+var transporter = nodemailer.createTransport("SMTP",{
+    service: 'Gmail',
+    auth: {
+        user: 'abhikori1994@gmail.com',
+        pass: 'abhishekori#gmailp@ssword1994'
+    }
+});
+
+// NB! No need to recreate the transporter object. You can use
+// the same transporter object for all e-mails
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: 'Fred Foo ✔ <abhikori1994@gmail>', // sender address
+    to: 'abhikori1994@gmail', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world ✔', // plaintext body
+    html: '<b>Hello world ✔</b>' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+
+});
